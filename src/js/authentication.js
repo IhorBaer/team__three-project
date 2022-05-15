44bed0baf164d801c82894882dab655860bb34bc
Vadym Tytarenko (Extractoro) <67538058+Extractoro@users.noreply.github.com>
2022-05-15 12:45:03 +0300

    remove pag from my lib

--- src/js/authentication.js
+++ src/js/authentication.js
@@ -1,9 +1,11 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore'
 import {
-    getAuth, createUserWithEmailAndPassword,
+    getAuth,
+    createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
-    getMultiFactorResolver, onAuthStateChanged
+    getMultiFactorResolver,
+    onAuthStateChanged
 } from 'firebase/auth'
 import Notiflix from 'notiflix';
 // import { movieApiService } from './render_movies';
@@ -21,15 +23,15 @@ export let userEmail = ""
 const dataData = new FetchApi()
 const modal = document.querySelector('.auth-modal')
 const openModal = document.querySelector('.open_auth-js')
-// console.log(openModal)
+    // console.log(openModal)
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgHWVD37iS9SyzyjybiROGSJgrZBuPF74",
-  authDomain: "fir-g3-a635e.firebaseapp.com",
-  projectId: "fir-g3-a635e",
-  storageBucket: "fir-g3-a635e.appspot.com",
-  messagingSenderId: "387248887615",
-  appId: "1:387248887615:web:53bf0176f3707f756ae58a"
+    apiKey: "AIzaSyCgHWVD37iS9SyzyjybiROGSJgrZBuPF74",
+    authDomain: "fir-g3-a635e.firebaseapp.com",
+    projectId: "fir-g3-a635e",
+    storageBucket: "fir-g3-a635e.appspot.com",
+    messagingSenderId: "387248887615",
+    appId: "1:387248887615:web:53bf0176f3707f756ae58a"
 };
 initializeApp(firebaseConfig)
 
@@ -45,53 +47,54 @@ const refLibrary = document.getElementById('library')
 const refHome = document.getElementById('home')
 const refWatchedBtn = document.getElementById('watched')
 const refQueueBtn = document.getElementById('queue')
-// const addQueueRef = document.getElementById('add-queue-js')
-// const addWatchedRef = document.getElementById('add-watched-js')
+    // const addQueueRef = document.getElementById('add-queue-js')
+    // const addWatchedRef = document.getElementById('add-watched-js')
 
 
 refWatchedBtn.addEventListener('click', (e) => {
     refWatchedBtn.setAttribute('disabled', true)
-    refQueueBtn .removeAttribute('disabled', true)
-  
+    refQueueBtn.removeAttribute('disabled', true)
+    refs.idPagination.classList.add('.visually-hidden')
+
     // refQueueBtn.textContent = "ff"
     films = []
-   getDocs(colRef)
-     .then((snapshot) => {
-         console.log(snapshot.docs)
-         snapshot.docs.forEach((doc) => {
-            films.push({ ...doc.data(), id: doc.id })
-      })
-             const dataLibrary = films.filter(film => film.status == 'watched' && film.user == userEmail)
+    getDocs(colRef)
+        .then((snapshot) => {
+            console.log(snapshot.docs)
+            snapshot.docs.forEach((doc) => {
+                films.push({...doc.data(), id: doc.id })
+            })
+            const dataLibrary = films.filter(film => film.status == 'watched' && film.user == userEmail)
 
-       renderListCard(dataLibrary)
-    })
-    .catch(err => {
-        console.log(err.message)
-       
-    }) 
- })
+            renderListCard(dataLibrary)
+        })
+        .catch(err => {
+            console.log(err.message)
+
+        })
+})
 
 refQueueBtn.addEventListener('click', (e) => {
-     refWatchedBtn.removeAttribute('disabled', true)
+    refWatchedBtn.removeAttribute('disabled', true)
     refQueueBtn.setAttribute('disabled', true)
-     films = []
-   getDocs(colRef)
-     .then((snapshot) => {
-         console.log(snapshot.docs)
-         snapshot.docs.forEach((doc) => {
-            films.push({ ...doc.data(), id: doc.id })
-      })
-             const dataLibrary = films.filter(film => film.status == 'queue' && film.user == userEmail)
+    films = []
+    getDocs(colRef)
+        .then((snapshot) => {
+            console.log(snapshot.docs)
+            snapshot.docs.forEach((doc) => {
+                films.push({...doc.data(), id: doc.id })
+            })
+            const dataLibrary = films.filter(film => film.status == 'queue' && film.user == userEmail)
 
-       renderListCard(dataLibrary)
-    })
-    .catch(err => {
-        console.log(err.message)
-       
-    }) 
- })
+            renderListCard(dataLibrary)
+        })
+        .catch(err => {
+            console.log(err.message)
+
+        })
+})
 refHome.addEventListener('click',
- 
+
     (e) => {
         refLibrary.removeAttribute('disabled', true)
         getPopularMoviesData()
@@ -101,35 +104,35 @@ refLibrary.addEventListener('click', (e) => {
     refLibrary.setAttribute('disabled', true)
     if (userEmail == false) {
         Notiflix.Notify.failure('LOG IN PLZ')
-                //    modal.classList.remove('visually-hidden');
+            //    modal.classList.remove('visually-hidden');
     }
     films = []
-   getDocs(colRef)
-     .then((snapshot) => {
-         console.log(snapshot.docs)
-         snapshot.docs.forEach((doc) => {
-            films.push({ ...doc.data(), id: doc.id })
-      })
-             const dataLibrary = films.filter(film => film.status == 'queue' && film.user == userEmail)
+    getDocs(colRef)
+        .then((snapshot) => {
+            console.log(snapshot.docs)
+            snapshot.docs.forEach((doc) => {
+                films.push({...doc.data(), id: doc.id })
+            })
+            const dataLibrary = films.filter(film => film.status == 'queue' && film.user == userEmail)
 
-       renderListCard(dataLibrary)
-    })
-    .catch(err => {
-        console.log(err.message)
-       
-    }) 
+            renderListCard(dataLibrary)
+        })
+        .catch(err => {
+            console.log(err.message)
+
+        })
 })
- 
-    document.getElementById("login").addEventListener('click', function () {
-        const email = document.getElementById('email').value
-        const password = document.getElementById('pass').value
+
+document.getElementById("login").addEventListener('click', function() {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('pass').value
 
 
 
-        signInWithEmailAndPassword(auth, email, password)
+    signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in 
-             userEmail = userCredential.user.email;
+            userEmail = userCredential.user.email;
             // console.log(userEmail)
             // ...
             Notiflix.Notify.info(`You are logged in ${email}`);
@@ -141,11 +144,11 @@ refLibrary.addEventListener('click', (e) => {
             const errorMessage = error.message;
             window.alert(errorCode + errorMessage);
         });
-     
-        
-    })
-        
-    document.getElementById("register").addEventListener('click', function(){
+
+
+})
+
+document.getElementById("register").addEventListener('click', function() {
     const email = document.getElementById('email').value
     const password = document.getElementById('pass').value
 
@@ -154,24 +157,24 @@ refLibrary.addEventListener('click', (e) => {
 
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-        // Signed in 
+            // Signed in 
             const user = userCredential.user;
             console.log(user)
-        // ...
+                // ...
             window.alert('Created')
-            // modal.classList.add('visually-hidden');
+                // modal.classList.add('visually-hidden');
         })
         .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-        window.alert(errorCode + errorMessage)
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+            window.alert(errorCode + errorMessage)
         });
-        
-    })
+
+})
 
 // export function renderListCardLibrary(data) {
- 
+
 //     const markup = itemsTemplate({...data });
 //     refs.gallery_films.innerHTML(markup);
 // }
