2df04b9aafb3aa0e51229af816aa96639098a07a
PetroTkach <petro.tka4444@gmail.com>
2022-05-16 16:57:24 +0300

    done

--- src/js/base/loader.js
+++ src/js/base/loader.js
@@ -4,3 +4,12 @@
 //   $(".loader").delay(400).fadeOut("slow");
  
 // });
+
+const loader = document.querySelector('.loader');
+console.log(loader);
+window.addEventListener('load', () => {
+  loader.classList.add('visually-hidden');
+  setTimeout(() => {
+    loader.remove();
+  }, 600);
+});
