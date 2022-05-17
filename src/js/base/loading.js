f2820593fc063300b5b8873b459453fa69265622
Vadym Tytarenko (Extractoro) <67538058+Extractoro@users.noreply.github.com>
2022-05-17 18:51:03 +0300

    Loader

---
+++ src/js/base/loading.js
@@ -0,0 +1,10 @@
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+
+Loading.init({
+    backgroundColor: 'rgba(0,0,0,0.25)',
+    svgSize: '110px',
+    svgColor: '#ff6b08',
+    clickToClose: false,
+});
+
+export default Loading;
